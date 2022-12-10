ea7cbe804ed350b412a83431c9fd476c8fcc6a72
Daniel Yue <yuedaniel@gmail.com>
2022-12-10 18:05:52 -0500

    refactored to support blueprints

--- app_old/models.py
+++ app_old/models.py
@@ -1,25 +1,22 @@
+from app import db, login, app
 from datetime import datetime
-from hashlib import md5
 from time import time
-from flask import current_app
-from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from hashlib import md5
 import jwt
-from app import db, login
 
-
-followers = db.Table(
-    'followers',
+followers = db.Table('followers',
     db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
     db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
 )
-
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    created_date = db.Column(db.DateTime, default=datetime.utcnow)
     posts = db.relationship('Post', backref='author', lazy='dynamic')
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
@@ -65,22 +62,20 @@ class User(UserMixin, db.Model):
     def get_reset_password_token(self, expires_in=600):
         return jwt.encode(
             {'reset_password': self.id, 'exp': time() + expires_in},
-            current_app.config['SECRET_KEY'], algorithm='HS256')
+            app.config['SECRET_KEY'], algorithm='HS256')
 
     @staticmethod
     def verify_reset_password_token(token):
         try:
-            id = jwt.decode(token, current_app.config['SECRET_KEY'],
+            id = jwt.decode(token, app.config['SECRET_KEY'],
                             algorithms=['HS256'])['reset_password']
         except:
             return
         return User.query.get(id)
 
-
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))
-
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
